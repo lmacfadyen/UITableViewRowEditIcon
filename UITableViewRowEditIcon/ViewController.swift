f9c9bb4f31136fda8ecf3f1a49b5ed2dcabc1222
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2015-10-28 15:37:13 -0400

    Initial Commit

---
+++ UITableViewRowEditIcon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableViewRowEditIcon
+//
+//  Created by Lawrence F MacFadyen on 2015-10-28.
+//  Copyright © 2015 LawrenceM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
